5c83e0605f2c6f6ce8f7abf42eec6c92c8ca26bd
Kevin Nicolas <bonjour@kevinnicolas.com>
2021-02-24 14:37:04 +0000

    Add function to render tweets dynamicly

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -37,17 +37,31 @@ const timeDifferenceFromNow = function(prev) {
   }
 };
 
-const tweetData = {
-  user: {
-    name: "Newton",
-    avatars: "https://i.imgur.com/73hZDYK.png",
-    handle: "@SirIsaac",
+const tweetData = [
+  {
+    user: {
+      name: "Newton",
+      avatars: "https://i.imgur.com/73hZDYK.png",
+      handle: "@SirIsaac",
+    },
+    content: {
+      text:
+        "If I have seen further it is by standing on the shoulders of giants",
+    },
+    created_at: 1461116232227,
   },
-  content: {
-    text: "If I have seen further it is by standing on the shoulders of giants",
+  {
+    user: {
+      name: "Descartes",
+      avatars: "https://i.imgur.com/nlhLi3I.png",
+      handle: "@rd",
+    },
+    content: {
+      text: "Je pense , donc je suis",
+    },
+    created_at: 1461113959088,
   },
-  created_at: 1461116232227,
-};
+];
 
 const createTweetElement = function(db) {
   const $tweet = $(`<article class="tweet">
@@ -73,7 +87,9 @@ const createTweetElement = function(db) {
   return $tweet;
 };
 
-const $tweet = createTweetElement(tweetData);
-
-console.log($tweet); // to see what it looks like
-$("#tweets-container").append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+const renderTweets = function(db) {
+  $.each(db, (index, tweetObj) => {
+    const $tweet = createTweetElement(tweetObj);
+    $("#tweets-container").append($tweet);
+  });
+};
